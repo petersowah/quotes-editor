9b2b760fe9d026cef031e638ccce54f9b89cbd25
Peter Sowah <petersowah@gmail.com>
2023-12-04 01:35:46 +0000

    wip

---
+++ test/controllers/quotes_controller_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class QuotesControllerTest < ActionDispatch::IntegrationTest
+  before_action :set_quote, only: %i[show edit update destroy]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show;end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quote_path, notice: "Quote was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quote_path, notice: "Quote was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
+end
--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,9 +1,46 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit quotes_url
-  #
-  #   assert_selector "h1", text: "Quotes"
-  # end
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New Quote"
+    assert_selector "h1", text: "New Quote"
+
+    fill_in "Quote", with: "This is a quote"
+    click_on "Create Quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "This is a quote"
+  end
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit quote"
+
+    fill_in "Name", with: "This is an updated quote"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "This is an updated quote"
+  end
+
+  test "Deleting a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end
 end
